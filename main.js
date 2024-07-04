c1fc60cb4c2ed439e4d8214cac9a53bea1007d44
erkay2707 <erkeaiym112707@gmail.com>
2024-07-04 16:59:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+//задание 1
+//let a = 'hello'
+ //let b = 'Ivan'
+//let c = a[0].toUpperCase()
+//let d = b[0].toLowerCase()
+//a = c + a.slice(1)
+//b = d + b.slice(1)
+//console.log(a + '' + b)
+
+//задание2
+//let a = 'lorem ipsum dolor'
+//a = a.length
+//console.log(a)
+
+//задание 3
+//let a = 'lorem ipsum dolor sit amet'
+//let b = a.slice(17,22)
+//let c = a.slice(0,17)
+//let d = a.slice(21)
+//let result = b + c + d
+
+//console.log(result)
+
+//задание 4
+//let a = 'children'
+//const str2 = a.slice(0, 7).toUpperCase()
+//const result = str2 + a[a.length - 1]
+//console.log('result', result)
+
+//задание 5
+//let a = 'boss'
+//let b = a.slice(0, 1)
+//let c = a.slice(2)
+//let d = a.slice(1,2)
+//let result = b + c + d
+
+//console.log(result)
+
+//задание 6
+// let a = 'sunday';
+// const result = a.indexOf('d');
+// console.log(result)
+
+// задание 7
+// let a = 'hello'
+// let b = a.slice(0,2) + 'ks' + a[4]
+//
+// console.log(b)
+
+//задание8
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0,2)
+// let result = b + ' ' + c
+// console.log(result)
+
+//задание 9
+// let a = 'monday'
+// let b = 'day'
+// let c = a.slice(0, 3)
+// let d = a.slice(3)
+// let result = b + d + c
+// console.log(result)
+
+//задание 10
+// let a = 'HELLO'
+// let b = a.slice(0, 1).toLowerCase()
+// let c = a.slice(1, 2).toUpperCase()
+// let d = 'I'
+// let e = a.slice(3, 4)
+// let f = a.slice(5).toLowerCase()
+// let g = a.slice(4).toLowerCase()
+// console.log(b + c + d + e + f + g)
+
+//задание 11
+// let a = 5
+// let b = '400'
+// console.log(450)
+
+//задание 12
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.slice(0, 1)
+// let d = b.slice(1, 2)
+// let e = a.slice(2, 3)
+// let f = b.slice(3, 4)
+// let g = a.slice(3, 4)
+// let result = c + d + e + f + g
+// console.log(result)
